a0d4c9ea2990692da2ba35a297f9332c33af49e2
kevgwa <kevingwanzyola@live.com>
2019-12-13 06:15:58 +0200

    Creation de la page d'acceuil du site

---
+++ index.php
@@ -0,0 +1,29 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>kevgwa</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Bienvenue sur notre site de vente en ligne du materiel informatique et des services informatique</h1>
+	<h2>MATERIELS INFORMATIQUES</h2>
+	<ul>
+		<li>IMPRIMANTE</li>
+		<li>DISQUE DUR</li>
+		<li>CABLE RJ45</li>
+		<li>FLASH DISQUE</li>
+		<li>ORDINATEUR PORTABLE</li>
+		<li>ENCRE POUR IMPRIMANTE</li>
+		<li>CARTOUCHES</li>
+		<li>CARTE MEMOIRE</li>
+	</ul>
+	<h2>SERVICES INFORMATIQUES</h2>
+	<ul>
+		<li>MAINTENANCE ORDINATEUR</li>
+		<li>MAINTENANCE IMPRIMANTE</li>
+		<li>SERVICES APRES VENTE</li>
+		<li></li>
+
+	</ul>
+</body>
+</html>
