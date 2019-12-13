d61615ab180931769a058eedb8641ee3b35d22ad
kevgwa <kevingwanzyola@live.com>
2019-12-13 06:41:51 +0200

    Ajout des sous-rubriques dans MATERIELS INFORMATIQUES

--- index.php
+++ index.php
@@ -5,15 +5,34 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<h1>Bienvenue sur notre site de vente en ligne du materiel informatique et des services informatique</h1>
+	<h1>Bienvenue sur notre site de vente en ligne du materiel informatique et des services informatiques</h1>
 	<h2>MATERIELS INFORMATIQUES</h2>
 	<ul>
 		<li>IMPRIMANTE</li>
+            <ul>
+                <li>LASER</li>
+                <li>A JET D'ENCRE</li>
+            </ul>
 		<li>DISQUE DUR</li>
+            <ul>
+                <li>SSD</li>
+                <li>HDD</li>
+            </ul>
 		<li>CABLE RJ45</li>
 		<li>FLASH DISQUE</li>
 		<li>ORDINATEUR PORTABLE</li>
+            <ul>
+                <li>ACER</li>
+                <li>HP</li>
+                <li>SONY</li>
+                <li>LENOVO/li>
+                <li>SAMSUNG</li>
+            </ul>
 		<li>ENCRE POUR IMPRIMANTE</li>
+            <ul>
+                <li>ENCRE LIQUIDE</li>
+                <li>ENCRE EN POUDRE - TONER</li>
+            </ul>
 		<li>CARTOUCHES</li>
 		<li>CARTE MEMOIRE</li>
 	</ul>
@@ -22,8 +41,6 @@
 		<li>MAINTENANCE ORDINATEUR</li>
 		<li>MAINTENANCE IMPRIMANTE</li>
 		<li>SERVICES APRES VENTE</li>
-		<li></li>
-
 	</ul>
 </body>
 </html>
